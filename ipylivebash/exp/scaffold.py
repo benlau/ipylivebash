44f8e4fb29b2f596fe101fedaaca904754a319b2
Ben Lau <xbenlau@gmail.com>
2023-08-26 12:58:31 +0800

    scaffold: Add EnvVar as a wrapper of Env Var

--- ipylivebash/exp/scaffold.py
+++ ipylivebash/exp/scaffold.py
@@ -1,12 +1,26 @@
 import asyncio
 import ipywidgets as widgets
 from IPython.display import display
+import os
 from ipylivebash.sessionmanager import run_script  # noqa
+
+
+class EnvVar:
+    """
+    Wrapper for environment variable
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def __call__(self, value, output):
+        os.environ[self.name] = value
+        output(f"Set {self.name}={value}")
 
 
 def _execute(script_or_callback, value, output_widget):
     if isinstance(script_or_callback, str):
-        script = script_or_callback.format(value=value)
+        script = script_or_callback
         output_widget.clear_output()
         env = {
             "LIVEBASH_VALUE": value,
@@ -18,10 +32,10 @@ def _execute(script_or_callback, value, output_widget):
         script_or_callback(value, output_widget.append_stdout)
 
 
-def select(label=None, options=None, run=None, action_label="Confirm"):
-    label_widget = None
-    if label is not None:
-        label_widget = widgets.Label(value=label)
+def select(title=None, options=None, run=None, action_label="Confirm"):
+    title_widget = None
+    if title is not None:
+        title_widget = widgets.Label(value=title)
 
     select_widget = widgets.Select(options=options)
     confirm_button = widgets.Button(description=action_label)
@@ -34,11 +48,24 @@ def select(label=None, options=None, run=None, action_label="Confirm"):
     confirm_button.on_click(confirm_button_callback)
 
     widgets_box = widgets.VBox(
-        [label_widget, select_widget, confirm_button, output_area]
+        [title_widget, select_widget, confirm_button, output_area]
     )
 
     return widgets_box
 
 
-def display_select(label, options, callback, **args):
-    display(select(label, options, callback, **args))
+def display_select(title, options, run, **args):
+    """
+    Show a select widget
+
+    Parameters
+    ----------
+    title: str
+        Title of the widget
+
+    options: list
+        List of options(string)
+
+    run: str or callable
+    """
+    display(select(title, options, run, **args))
